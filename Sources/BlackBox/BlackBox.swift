24e0c83e125bce6e694da21de11f11b19e2a9390
Aleksey Berezka <a.berezka@dodopizza.com>
2022-10-01 22:46:59 +0500

    Added documentation to BlackBox static methods

--- Sources/BlackBox/BlackBox.swift
+++ Sources/BlackBox/BlackBox.swift
@@ -170,6 +170,17 @@ extension BlackBox: BBProtocol {
 
 // MARK: - Static
 extension BlackBox {
+    /// Logs plain message
+    /// - Parameters:
+    ///   - message: Message to log
+    ///   - userInfo: Additional info you'd like to see alongside log
+    ///   - serviceInfo: to be deleted
+    ///   - logLevel: level of log
+    ///   - category: Category of log. E.g. View Lifecycle.
+    ///   - parentEvent: Parent log of current log. May be useful for traces.
+    ///   - fileID: The fileID where the logs occurs. Containts module name and filename. The default is the fileID of the function where you call log.
+    ///   - function: The function where the logs occurs. The default is the function name from where you call log.
+    ///   - line: The line where the logs occurs. The default is the line in function from where you call log.
     public static func log(
         _ message: String,
         userInfo: BBUserInfo? = nil,
@@ -194,6 +205,15 @@ extension BlackBox {
         )
     }
     
+    /// Logs error messages
+    /// - Parameters:
+    ///   - error: Error to log
+    ///   - serviceInfo: to be deleted
+    ///   - category: Category of log. E.g. View Lifecycle.
+    ///   - parentEvent: Parent log of current log. May be useful for traces.
+    ///   - fileID: The fileID where the logs occurs. Containts module name and filename. The default is the fileID of the function where you call log.
+    ///   - function: The function where the logs occurs. The default is the function name from where you call log.
+    ///   - line: The line where the logs occurs. The default is the line in function from where you call log.
     public static func log(
         _ error: Swift.Error,
         serviceInfo: BBServiceInfo? = nil,
@@ -213,7 +233,20 @@ extension BlackBox {
             line: line
         )
     }
-    
+
+    // MARK: - Trace
+    /// Logs measurement start
+    /// - Parameters:
+    ///   - message: Measurement name
+    ///   - userInfo: Additional info you'd like to see alongside log
+    ///   - serviceInfo: to be deleted
+    ///   - logLevel: level of log
+    ///   - category: Category of log. E.g. View Lifecycle.
+    ///   - parentEvent: Parent log of current log. May be useful for traces.
+    ///   - fileID: The fileID where the logs occurs. Containts module name and filename. The default is the fileID of the function where you call log.
+    ///   - function: The function where the logs occurs. The default is the function name from where you call log.
+    ///   - line: The line where the logs occurs. The default is the line in function from where you call log.
+    /// - Returns: Started measurement
     public static func logStart(
         _ message: String,
         userInfo: BBUserInfo? = nil,
@@ -238,12 +271,25 @@ extension BlackBox {
         )
     }
     
+    /// Logs measurement start
+    /// - Parameter event: measurement start event
     public static func logStart(
         _ event: StartEvent
     ) {
         BlackBox.instance.logStart(event)
     }
     
+    /// Logs measurement end
+    /// - Parameters:
+    ///   - startEvent: Measurement start event
+    ///   - message: Alternate message to log instead of ``BlackBox.StartEvent`` message.
+    ///   - userInfo: Additional info you'd like to see alongside log
+    ///   - serviceInfo: to be deleted
+    ///   - category: Category of log. E.g. View Lifecycle.
+    ///   - parentEvent: Parent log of current log. May be useful for traces.
+    ///   - fileID: The fileID where the logs occurs. Containts module name and filename. The default is the fileID of the function where you call log.
+    ///   - function: The function where the logs occurs. The default is the function name from where you call log.
+    ///   - line: The line where the logs occurs. The default is the line in function from where you call log.
     public static func logEnd(
         _ event: StartEvent,
         message: String? = nil,
