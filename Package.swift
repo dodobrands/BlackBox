d569afe4986763f65159c3cb3627fd8726e83631
Aleksey Berezka <a.berezka@dodopizza.com>
2022-10-03 20:01:10 +0600

    Added tvOS support

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "BlackBox",
     platforms: [
         .iOS(.v12),
-        .macOS(.v10_14)
+        .macOS(.v10_14),
+        .tvOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
