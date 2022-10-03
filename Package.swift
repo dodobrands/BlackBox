cf0e1e63e3b077105923eda1a9b4e7f47ce27729
Aleksey Berezka <a.berezka@dodopizza.com>
2022-10-03 20:00:18 +0600

    Added macOS support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "BlackBox",
-    platforms: [.iOS(.v12)],
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_14)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
