818bee630680db769abc2c0f17d9b3fc05a8ad37
Aleksey Berezka <a.berezka@dodopizza.com>
2022-10-14 22:13:29 +0600

    Simplified tests naming

--- Tests/BlackBoxTests/BlackBoxErrorEventTests.swift
+++ Tests/BlackBoxTests/BlackBoxErrorEventTests.swift
@@ -10,75 +10,75 @@ import XCTest
 @testable import ExampleModule
 
 class BlackBoxErrorEventTests: BlackBoxTestCase {
-    func test_errorLog_error() {
+    func test_error() {
         log(AnakinKills.maceWindu)
         XCTAssertEqual(logger.errorEvent?.error as? AnakinKills, AnakinKills.maceWindu)
     }
     
-    func test_errorLog_message() {
+    func test_message() {
         log(AnakinKills.maceWindu)
         XCTAssertEqual(logger.errorEvent?.message, "BlackBoxTests.AnakinKills.maceWindu")
     }
     
-    func test_errorLog_message_fromAnotherModule() {
+    func test_message_fromAnotherModule() {
         waitForLog {
             ExampleService().logSomeError()
         }
         XCTAssertEqual(logger.errorEvent?.message, "ExampleModule.ExampleError.taskFailed")
     }
     
-    func test_errorLog_messageOfErrorWithAssociatedValue() {
+    func test_messageOfErrorWithAssociatedValue() {
         log(AnakinKills.younglings(count: 11))
         XCTAssertEqual(logger.errorEvent?.message, "BlackBoxTests.AnakinKills.younglings")
     }
     
-    func test_errorLog_userInfoOfErrorWithAssociatedValue() {
+    func test_userInfoOfErrorWithAssociatedValue() {
         log(AnakinKills.younglings(count: 11))
         XCTAssertEqual(logger.errorEvent?.userInfo as? [String: Int], ["count": 11])
     }
     
-    func test_errorLog_serviceInfo() {
+    func test_serviceInfo() {
         log(AnakinKills.maceWindu, serviceInfo: Lightsaber(color: "purple"))
         XCTAssertEqual(logger.errorEvent?.serviceInfo as? Lightsaber, Lightsaber(color: "purple"))
     }
     
-    func test_errorLog_defaultLevel() {
+    func test_defaultLevel() {
         log(AnakinKills.maceWindu)
         XCTAssertEqual(logger.errorEvent?.level, .error)
     }
     
-    func test_errorLog_category() {
+    func test_category() {
         log(AnakinKills.maceWindu, category: "Analytics")
         XCTAssertEqual(logger.errorEvent?.category, "Analytics")
     }
     
-    func test_errorLog_parentEvent() {
+    func test_parentEvent() {
         let parentEvent = BlackBox.GenericEvent("Test")
         log(AnakinKills.maceWindu, parentEvent: parentEvent)
         XCTAssertEqual(logger.errorEvent?.parentEvent, parentEvent)
     }
     
-    func test_errorLog_fileID() {
+    func test_fileID() {
         log(AnakinKills.maceWindu)
         XCTAssertEqual(logger.errorEvent?.source.fileID.description, "BlackBoxTests/BlackBoxErrorEventTests.swift")
     }
     
-    func test_errorLog_module() {
+    func test_module() {
         log(AnakinKills.maceWindu)
         XCTAssertEqual(logger.errorEvent?.source.module, "BlackBoxTests")
     }
     
-    func test_errorLog_filename() {
+    func test_filename() {
         log(AnakinKills.maceWindu)
         XCTAssertEqual(logger.errorEvent?.source.filename, "BlackBoxErrorEventTests")
     }
     
-    func test_errorLog_function() {
+    func test_function() {
         log(AnakinKills.maceWindu)
-        XCTAssertEqual(logger.errorEvent?.source.function.description, "test_errorLog_function()")
+        XCTAssertEqual(logger.errorEvent?.source.function.description, "test_function()")
     }
     
-    func test_errorLog_line() {
+    func test_line() {
         log(AnakinKills.maceWindu)
         XCTAssertEqual(logger.errorEvent?.source.line, 82)
     }
--- Tests/BlackBoxTests/BlackBoxStartEventTests.swift
+++ Tests/BlackBoxTests/BlackBoxStartEventTests.swift
@@ -14,68 +14,68 @@ class BlackBoxStartEventTests: BlackBoxTestCase {
         logStart(event)
         XCTAssertEqual(logger.startEvent, event)
     }
-    func test_startLog_message() {
+    func test_message() {
         logStart("Test")
         XCTAssertEqual(logger.startEvent?.message, "Start: Test")
     }
     
-    func test_startLog_rawMessage() {
+    func test_rawMessage() {
         logStart("Test")
         XCTAssertEqual(logger.startEvent?.rawMessage, "Test")
     }
     
-    func test_startLog_userInfo() {
+    func test_userInfo() {
         logStart("Test", userInfo: ["name": "Kenobi"])
         XCTAssertEqual(logger.startEvent?.userInfo as? [String: String], ["name": "Kenobi"])
     }
     
-    func test_startLog_serviceInfo() {
+    func test_serviceInfo() {
         logStart("Test", serviceInfo: Lightsaber(color: "purple"))
         XCTAssertEqual(logger.startEvent?.serviceInfo as? Lightsaber, Lightsaber(color: "purple"))
     }
     
-    func test_startLog_level() {
+    func test_level() {
         logStart("Test", level: .warning)
         XCTAssertEqual(logger.startEvent?.level, .warning)
     }
     
-    func test_startLog_defaultLevel() {
+    func test_defaultLevel() {
         logStart("Test")
         XCTAssertEqual(logger.startEvent?.level, .debug)
     }
     
-    func test_startLog_category() {
+    func test_category() {
         logStart("Test", category: "Analytics")
         XCTAssertEqual(logger.startEvent?.category, "Analytics")
     }
     
-    func test_startLog_parentEvent() {
+    func test_parentEvent() {
         let parentEvent = BlackBox.StartEvent("Test")
         logStart("Test 2", parentEvent: parentEvent)
         XCTAssertEqual(logger.startEvent?.parentEvent, parentEvent)
     }
     
-    func test_startLog_fileID() {
+    func test_fileID() {
         logStart("Test")
         XCTAssertEqual(logger.startEvent?.source.fileID.description, "BlackBoxTests/BlackBoxStartEventTests.swift")
     }
     
-    func test_startLog_module() {
+    func test_module() {
         logStart("Test")
         XCTAssertEqual(logger.startEvent?.source.module, "BlackBoxTests")
     }
     
-    func test_startLog_filename() {
+    func test_filename() {
         logStart("Test")
         XCTAssertEqual(logger.startEvent?.source.filename, "BlackBoxStartEventTests")
     }
     
-    func test_startLog_function() {
+    func test_function() {
         logStart("Test")
-        XCTAssertEqual(logger.startEvent?.source.function.description, "test_startLog_function()")
+        XCTAssertEqual(logger.startEvent?.source.function.description, "test_function()")
     }
     
-    func test_startLog_line() {
+    func test_line() {
         logStart("Test")
         XCTAssertEqual(logger.startEvent?.source.line, 79)
     }
