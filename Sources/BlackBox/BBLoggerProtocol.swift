07908d08ff57c7e5bf27afdb657fdf5f9231d202
Aleksey Berezka <a.berezka@dodopizza.com>
2022-10-18 08:58:45 +0600

    Removed headers

--- Sources/BlackBox/BBLoggerProtocol.swift
+++ Sources/BlackBox/BBLoggerProtocol.swift
@@ -1,11 +1,3 @@
-//
-//  BBLoggerProtocol.swift
-//  DFoundation
-//
-//  Created by Алексей Берёзка on 27.02.2020.
-//  Copyright © 2020 ru.dodopizza. All rights reserved.
-//
-
 import Foundation
 
 public protocol BBLoggerProtocol {
