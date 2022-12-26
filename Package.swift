c7bb6abee1acb1b3b4c23eeec1d6ae89a79f93cf
Aleksey Berezka <a.berezka@dodopizza.com>
2022-12-26 12:10:19 +0700

    Added swift-docc-plugin to project

--- Package.swift
+++ Package.swift
@@ -26,7 +26,9 @@ let package = Package(
             ]
         ),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+    ],
     targets: [
         .target(
             name: targetName,
